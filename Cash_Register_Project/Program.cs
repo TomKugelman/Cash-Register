3207fbedef01d93819efbfa67f6f6bc4ad1f56d4
TomKugelman <52869328+TomKugelman@users.noreply.github.com>
2019-12-24 15:08:11 -0500

    Creation

---
+++ Cash_Register_Project/Program.cs
@@ -0,0 +1,18 @@
+﻿// Author: Tom Kugelman
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cash_Register_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This is a Cash Register Program that runs in the console
+        }
+    }
+}
