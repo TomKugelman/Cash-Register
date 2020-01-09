948dffbd6f3fe23415ffbb7b12d8b1e8d3f22ef5
TomKugelman <52869328+TomKugelman@users.noreply.github.com>
2020-01-09 15:39:16 -0500

    Fixed calculations, created input validation, fixed outputs, tested cases

--- Cash_Register_Project/Program.cs
+++ Cash_Register_Project/Program.cs
@@ -14,83 +14,166 @@ namespace Cash_Register_Project
         {
             #region Data Members
             // user generated variables
-            float total;
-            float payment;
-            float change;
+            double total = 0;
+            double payment = 0;
+            double totalChange;
+            double change;
 
             // coin counters for chnage
             int penny = 0;
+            int nickel = 0;
             int dime = 0;
             int quarter = 0;
             int dollar = 0;
+            int fiveDollar = 0;
+            int tenDollar = 0;
+            int twentyDollar = 0;
 
-            // temp doubel variables to be truncated after division
+            // temp float variables to be truncated after division
             double pennyTemp;
+            double nickelTemp;
             double dimeTemp;
             double quarterTemp;
-
-            // Track the amoutn of change
-            double cents = 0;
+            double dollarTemp;
+            double fiveTemp;
+            double tenTemp;
+            double twentyTemp;
             #endregion
 
+            #region Get inputs
+            // input verification
+            string input1;
+            string input2;
+            bool goodInput1 = false;
+            bool goodInput2 = false;
             // get total from user
-            Console.WriteLine("Dollar amount of purchase (ex: 14.25)");
-            total = float.Parse(Console.ReadLine());
-
+            while (goodInput1 == false)
+            {
+                Console.WriteLine("Dollar amount of purchase (ex: 14.25)");
+                input1 = Console.ReadLine();
+                goodInput1 = double.TryParse(input1, out total);
+                if (!goodInput1)
+                    Console.WriteLine("Invalid input");
+            }
+            
             // get payment from user
-            Console.WriteLine("Dollar amount the customer gave");
-            payment = float.Parse(Console.ReadLine());
+            
+            while (goodInput2 == false)
+            {
+                Console.WriteLine("Dollar amount the customer gave");
+                input2 = Console.ReadLine();
+                goodInput2 = double.TryParse(input2, out payment);
+                if (payment < total)
+                {
+                    goodInput2 = false;
+                    Console.WriteLine("The customer did not give enough moeny to cover the bill");
+                }
+            }
 
             // calculate change
-            change = total - payment;
+            totalChange = payment - total;
+            change = totalChange;
+            #endregion
 
-            // split leftover change into dollar amounts and coin amounts
+            // split leftover change into dollar amounts then coin amounts
             // casting to int from double whenever truncation is required
-            if (change > 1)
+            // this allows me to skip having to manually truncate each "chnage" amount when 
+            // converting to an integer for the final output. However, it forces me to use a temp variable
+            // to do the calculation first, then cast into an integer, so there is a trade off
+            #region Calculate Dollar Returns
+            while (change > 1)
             {
-                cents = change % 1;
-
-                dollar = (int)change;
+                if (change >= 20)
+                {
+                    twentyTemp = change / 20;
+                    twentyDollar = (int)twentyTemp;
+                    change %= 20;
+                    change = Math.Round(change, 2);
+                }
+                else if (change >= 10)
+                {
+                    tenTemp = change / 10;
+                    tenDollar = (int)tenTemp;
+                    change %= 10;
+                    change = Math.Round(change, 2);
+                }
+                else if (change >= 5)
+                {
+                    fiveTemp = change / 5;
+                    fiveDollar = (int)fiveTemp;
+                    change %= 5;
+                    change = Math.Round(change, 2);
+                }
+                else
+                {
+                    dollarTemp = change / 1;
+                    dollar = (int)dollarTemp;
+                    change %= 1;
+                    change = Math.Round(change, 2);
+                    break;
+                }
+                
             }
-            else if (change > 0)
-            {
-                cents = change % 1;
-            }
-            else if (change == 0)
-                Console.WriteLine("The customer gave exact change!");
+            #endregion
 
             // calculate amount of each coin type to be returned to customer
             #region Calculate Coin Returns
-            while (cents > 0)
+            while (change > 0.00)
             {
-                if (cents > .25)
+                if (change >= .25)
                 {
-                    quarterTemp = cents / .25;
+                    quarterTemp = change / .25;
                     quarter = (int)quarterTemp;
-                    cents %= .25;
+                    change %= .25;
+                    change = Math.Round(change, 2);
                 }
-                else if (cents > .1)
+                else if (change >= .1)
                 {
-                    dimeTemp = cents / .1;
+                    dimeTemp = change / .1;
                     dime = (int)dimeTemp;
-                    cents %= .1;
+                    change %= .1;
+                    change = Math.Round(change, 2);
                 }
-                else if (cents > .01)
+                else if (change >= .05)
                 {
-                    pennyTemp = cents / .01;
-                    penny = (int)pennyTemp;
-                    cents /= .01;
+                    nickelTemp = change / .05;
+                    nickel = (int)nickelTemp;
+                    change %= .05;
+                    change = Math.Round(change, 2);
                 }
+                else
+                {
+                    pennyTemp = change / .01;
+                    penny = (int)pennyTemp;
+                    change %= .01;
+                    break;
+                }
+                
             }
+            #endregion
+
+            #region Output dollar and coin returns
+            Console.WriteLine();
+            Console.WriteLine("total change due back: " + totalChange.ToString());
+            Console.WriteLine();
+            if (twentyDollar >= 1)
+                Console.WriteLine(twentyDollar.ToString() + ": Twenty dollar bill(s)");
+            if (tenDollar >= 1)
+                Console.WriteLine(tenDollar.ToString() + ": Ten dollar bill(s)");
+            if (fiveDollar >= 1)
+                Console.WriteLine(fiveDollar.ToString() + ": Five dollar bill(s)");
+            if (dollar >= 1)
+                Console.WriteLine(dollar.ToString() + ": One dollar bill(s)");
+            if (quarter >= 1)
+                Console.WriteLine(quarter.ToString() + ": quarter(s)");
+            if (dime >= 1)
+                Console.WriteLine(dime.ToString() + ": dime(s)");
+            if (nickel >= 1)
+                Console.WriteLine(nickel.ToString() + ": nickel(s)");
+            if (penny >= 1)
+                Console.WriteLine(penny.ToString() + ": penny(s)");
+            Console.ReadLine();
             #endregion 
-
-            Console.WriteLine("total change due back: " + change.ToString());
-            Console.WriteLine((dollar >= 1) ? dollar.ToString() + ": Dollars, " :
-                (quarter >= 1) ? quarter.ToString() + ": Quarters, " : 
-                (dime >= 1) ? dime.ToString() + ": Dimes, " :
-                (penny >= 1) ? penny.ToString() + ": Pennies");
-
-            Console.WriteLine((dollar >= 1) ? dollar.ToString() + ": Dollars, " : "";
         }
     }
 }
